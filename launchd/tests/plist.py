cdb1a0173a20f1e7601209dc4ae1989c686f1dfd
Christian Clauss <cclauss@me.com>
2023-04-26 07:58:00 +0200

    Test current versions of Python on multiple macOS versions (#12)
    * tox.ini: Test current versions of Python
    
    https://devguide.python.org/versions
    
    * tests.yml: matrix.os: [macos-10.15, macos-11, macos-12]
    
    * Update tests.yml
    
    * setup.cfg: Allow example.py to print()
    
    * Fail fast if plist.discover_filename() returns None
    
    * com.apple.kextd was removed in macOS v11

--- launchd/tests/plist.py
+++ launchd/tests/plist.py
@@ -38,18 +38,22 @@ class PlistToolTest(unittest.TestCase):
         self.assertTrue(fname.startswith("/System/Library/LaunchDaemons/"))
 
     @unittest.skipUnless(sys.platform.startswith("darwin"), "requires OS X")
+    @unittest.skipIf(sys.version_info >= (3, 11), "com.apple.kextd was removed in macOS v11")
     def test_discover_filename(self):
         fname = plist.discover_filename("com.apple.kextd", plist.DAEMON_OS)
+        self.assertIsNotNone(fname)
         self.assertTrue(os.path.isfile(fname))
 
         # no scope specified
         fname2 = plist.discover_filename("com.apple.kextd")
+        self.assertIsNotNone(fname)
         self.assertTrue(os.path.isfile(fname2))
 
         fname = plist.discover_filename("com.apple.kextd", plist.USER)
-        self.assertEqual(None, fname)
+        self.assertIsNone(fname)
 
     @unittest.skipUnless(sys.platform.startswith("darwin"), "requires OS X")
+    @unittest.skipIf(sys.version_info >= (3, 11), "com.apple.kextd was removed in macOS v11")
     def test_read(self):
         result = plist.read("com.apple.kextd", plist.DAEMON_OS)
         self.assertIsInstance(result, dict)
